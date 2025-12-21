c07ae032de39ec5a814921fb84e69d883a6ef6c4
Maula Ibrahim Syahhwi <boimmuhammad8@gmail.com>
2025-12-21 23:48:15 +0700

    code splitting menggunakan manualChunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("firebase")) {
+              return "firebase";
+            }
+            if (
+              id.includes("react") ||
+              id.includes("react-dom") ||
+              id.includes("react-router-dom")
+            ) {
+              return "react-vendor";
+            }
+            if (
+              id.includes("styled-components") ||
+              id.includes("@hello-pangea/dnd") ||
+              id.includes("lucide-react")
+            ) {
+              return "ui-vendor";
+            }
+            return "vendor";
+          }
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000,
+  },
+});
